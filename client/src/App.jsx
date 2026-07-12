6173dc6fa566a381e726af0f8f28e81b84662fd4
Mahad-Ghauri <mahadghauri222@gmail.com>
2026-07-12 07:34:11 +0500

    refactor: update dependencies and restructure application layout
    - Added new font dependencies: @fontsource-variable/inter and @fontsource/ibm-plex-mono.
    - Introduced KaTeX for rendering mathematical expressions.
    - Removed unused App.css file to streamline styles.
    - Refactored App.jsx to include new components: TopBar, NavRail, and updated routing for new pages (NetworkCanvas, ActivationLab, OptimizerArena, CnnLab).
    - Deleted tailwind.config.js as part of the cleanup.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ActivationFunctions from './pages/ActivationFunctions';
-// Import other pages as they are built...
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TopBar from "./components/layout/TopBar";
+import NavRail from "./components/layout/NavRail";
+import { EngineProvider } from "./lib/useEngine";
+import { ToastProvider } from "./components/ui/Toast";
+import NetworkCanvas from "./pages/NetworkCanvas";
+import ActivationLab from "./pages/ActivationLab";
+import OptimizerArena from "./pages/OptimizerArena";
+import CnnLab from "./pages/CnnLab";
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/activation" element={<ActivationFunctions />} />
-      </Routes>
-    </Router>
+    <EngineProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <div className="flex h-full flex-col">
+            <TopBar />
+            <div className="flex min-h-0 flex-1 max-lg:flex-col-reverse lg:flex-row">
+              <NavRail />
+              <Routes>
+                <Route path="/" element={<NetworkCanvas />} />
+                <Route path="/activations" element={<ActivationLab />} />
+                <Route path="/optimizers" element={<OptimizerArena />} />
+                <Route path="/cnn" element={<CnnLab />} />
+              </Routes>
+            </div>
+          </div>
+        </BrowserRouter>
+      </ToastProvider>
+    </EngineProvider>
   );
 }
-
-export default App;
